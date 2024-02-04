556685b0478379e69db7ed8e673271a597230834
Shivam Jha <42311911+HUNTER1622@users.noreply.github.com>
2024-02-04 07:27:01 +0000

    4 feb

---
+++ Js_Basics001/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 
+    +
+     3) // code readability should be high
+
+console.log("Hitesh")
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value // empty value (ye ek khali value hn agr mujhe temperature lena hn or koi temp nai mila to as a server response mujhe null milega 0 nai )
+// undefined => 
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
